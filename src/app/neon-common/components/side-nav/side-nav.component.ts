f33ac4c506118584881ffabcf9125828bb896f3b
skycader <skycader@yandex.ru>
2024-01-28 04:40:21 +0300

    develop: presave

--- src/app/neon-common/components/side-nav/side-nav.component.ts
+++ src/app/neon-common/components/side-nav/side-nav.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NavbarService } from '../../services/navbar.service';
 
 @Component({
   selector: 'neon-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.scss'
+  styleUrl: './side-nav.component.scss',
 })
 export class SideNavComponent {
+  public get isSideNav() {
+    return this.navbarService.isSideNav;
+  }
+  constructor(private navbarService: NavbarService) { }
 
+  public toggleSideNav() {
+    this.navbarService.toggleSideNav();
+  }
 }
